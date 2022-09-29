c37be0a0b81d028cc3fbf583e4ed767e5e8c9d40
Daniel Williams <danielpaulwill@gmail.com>
2022-09-28 22:10:25 -0400

    LoadingPage added

---
+++ client/src/LoadingPage.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+
+function LoadingPage() {
+  
+  return (
+      <div className="center">
+        <h2>Loading...</h2>
+      </div>
+  )
+};
+
+export default LoadingPage;
