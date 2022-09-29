d66c2201425afc75904264fb0e2aa58d71d49922
Daniel Williams <danielpaulwill@gmail.com>
2022-09-28 22:15:21 -0400

    Signup added

---
+++ client/src/Signup.js
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+
+function Signup({ handleSignupClick, handleLoginClick, errors, onUsername, onPassword }) {
+  const [signupUsername, setUsername] = useState('')
+  const [signupPassword, setPassword] = useState('')
+
+  function onSignupClick(e) {
+    e.preventDefault()
+    handleSignupClick(signupUsername, signupPassword)
+  }
+
+  return (
+    <div id="welcome">
+      <h3>Signup</h3>
+      <form>
+        <input className="textInput" type="text" placeholder="username" onChange={e => setUsername(e.target.value)}></input>
+        <br></br>
+        <br></br>
+        <input className="textInput" type="password" placeholder="password" onChange={e => setPassword(e.target.value)}></input>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <button className="normalButton" onClick={onSignupClick}>Sign up</button>
+      </form>
+      <p className={(errors === '') ? 'errors2' : 'errors1'}>{errors}</p>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div>
+        <p>Already signed up?</p>
+        <button className="normalButton" onClick={handleLoginClick}>Go to Login</button>
+      </div>
+    </div>
+  )
+};
+
+export default Signup;
