4b20232fea53ad34c109618854b69a71bea3ebe8
Daniel Williams <danielpaulwill@gmail.com>
2022-09-28 21:54:48 -0400

    updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_one :character
+  has_many :locations, through: :character
+  has_many :events, through: :locations
+
+  has_secure_password
+  # validates :password, confirmation: { case_sensitive: true }
+  validates :username, presence: true, uniqueness: true 
 end
