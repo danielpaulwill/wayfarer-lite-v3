cf4a0861988499434a5b330b7ed92d21fb2d1dfd
Daniel Williams <danielpaulwill@gmail.com>
2022-09-28 22:14:40 -0400

    SetupContainer created

---
+++ client/src/SetupContainer.js
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from "react";
+import GameStart from "./GameStart";
+import ChooseYourCharacter from "./ChooseYourCharacter";
+import ConfirmGameBegin from "./ConfirmGameBegin";
+import LoadingPage from "./LoadingPage";
+
+function SetupContainer({ user, character, characterName, characterAvatar, handleNameChange, handleAvatarClick, handleProfessionChange, handleCharacterConfirm, handleMapView, archerAvatar, mageAvatar, warriorAvatar, health, evil, strength, defense, luck, setCharacterAttributes, setCharacter, setupWorkaround, characterErrors }) {
+  
+  const gameStart = <GameStart handleClick={handleStartClick} />
+  const confirmGameBegin = <ConfirmGameBegin locationSeed={locationSeed} />
+  const chooseYourCharacter = <ChooseYourCharacter handleNameChange={handleNameChange} handleAvatarClick={handleAvatarClick} handleProfessionChange={handleProfessionChange} handleCharacterConfirm={handleCharacterConfirm} characterName={characterName} characterAvatar={characterAvatar} archerAvatar={archerAvatar} mageAvatar={mageAvatar} warriorAvatar={warriorAvatar} setCharacter={setCharacter} characterErrors={characterErrors} />
+  const loadingPage = <LoadingPage />
+
+  const [currentPage, setCurrentPage] = useState(gameStart)
+
+
+  function handleStartClick(e) {
+    setCurrentPage(chooseYourCharacter)
+    window.scrollTo(0, 0);
+  }
+
+  useEffect(() => {
+    if (setupWorkaround === "confirmBegin") {
+      setCurrentPage(confirmGameBegin)
+      window.scrollTo(0, 0);
+    }
+  }, [setupWorkaround])
+
+  // function onCharacterConfirm() {
+  // }
+
+  function locationSeed() {
+    setCurrentPage(loadingPage)
+    // locations POST
+    fetch('/locations', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify([]),
+    })
+      .then((res) => {
+        if (res.ok) {
+          console.log("Locations POSTed")
+        }})
+
+    setTimeout(function(){ 
+      // events POST
+      fetch('/events', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify([]),
+      })
+        .then((res) => {
+          if (res.ok) {
+            console.log("Events POSTed")
+            
+          }})
+
+      }, 1000);
+
+
+    setTimeout(function(){
+      // options Post
+      fetch('/options', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify([]),
+      })
+        .then((res) => {
+          if (res.ok) {
+            console.log("Options POSTed")
+          }})
+    }, 2000);
+
+
+    setTimeout(function() {
+      handleMapView()
+    }, 3000)
+
+  }
+
+  return (
+    <div className="gameArea">
+      {currentPage}
+    </div>
+  )
+};
+
+export default SetupContainer;
