325f6b70fb50d14c21f578a823e9fb59a1c5c314
Daniel Williams <danielpaulwill@gmail.com>
2022-09-28 22:07:55 -0400

    index updated

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Signup from './Signup';
+import GameContainer from './GameContainer';
+import Login from './Login';
+import LandingPage from './LandingPage';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+        <Route path='*' element={<App />} />
+    </Routes>
+  </BrowserRouter>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
