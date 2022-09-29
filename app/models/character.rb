80be8ccd2f829e16404f8a55b26ffff6620fb871
Daniel Williams <danielpaulwill@gmail.com>
2022-09-28 21:52:29 -0400

    character model updated

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,10 @@
 class Character < ApplicationRecord
+  has_many :items
+  has_many :locations
+  has_many :events, through: :locations
+  belongs_to :user
+  validates :name, presence: true
+  validates :avatar, presence: true
+  validates :health, :presence => { message: "... Select a profession"}
+  validates_uniqueness_of :name, scope: :user_id
 end
