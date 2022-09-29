df7e0db0a6c68c89a009b951ed0da084c4bbbd6e
Daniel Williams <danielpaulwill@gmail.com>
2022-09-28 22:18:10 -0400

    all files migrated

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,14 @@ Rails.application.routes.draw do
   resources :items
   resources :locations
   resources :options
-  resources :users
+  resources :users, except: :show
+  get '/me', to: 'users#show'
+  post '/me', to: 'sessions#create'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  post '/character-attributes', to: 'character_attributes#create'
+  post '/locations-select', to: 'locations#select'
+  post '/locations-select-again', to: 'locations#select_again'
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
