0a765d2363ea8bbc75db7964624ed48a62fbc71b
Daniel Williams <danielpaulwill@gmail.com>
2022-10-04 15:39:41 -0400

    routing issues fixed

--- client/src/index.js
+++ client/src/index.js
@@ -6,16 +6,27 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   // <React.StrictMode>
   <BrowserRouter>
     <Routes>
         <Route path='*' element={<App />} />
     </Routes>
-  </BrowserRouter>
+  </BrowserRouter>,
+  document.getElementById("root")
   // </React.StrictMode>
 );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   // <React.StrictMode>
+//   <BrowserRouter>
+//     <Routes>
+//         <Route path='*' element={<App />} />
+//     </Routes>
+//   </BrowserRouter>
+//   // </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
