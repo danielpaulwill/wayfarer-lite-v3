b3932a5ca14926088fdc3243b6999ee212614aad
Daniel Williams <danielpaulwill@gmail.com>
2022-10-07 10:40:55 -0400

    added logo and animated button

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 function LandingPage({ handleClick }) {
+  const [visibleButton, setVisibleButton] = useState(true)
+  const [invertedButton, setInvertedButton] = useState(true)
+
+  useEffect(() => {
+    setTimeout(buttonAppear, 3000)
+  }, [])
+
+  useEffect(() => {
+    setTimeout(buttonInvert, 500)
+  }, [invertedButton])
+
+  function buttonAppear() {
+    setVisibleButton(visibleButton => !visibleButton)
+  }
+
+  function buttonInvert() {
+    setInvertedButton(invertedButton => !invertedButton)
+  }
 
   return (
     <div id="welcome">
-      <h1>GAME LOGO HERE</h1>
-      <div>
-        <button id="welcomeButton" onClick={handleClick}>Click me to begin!</button>
+      <h1>WELCOME TO</h1>
+      <img id="logo" src="https://raw.githubusercontent.com/danielpaulwill/wayfarer-lite-v3/main/client/src/assets/pixelated-color-logo.png"></img>
+      <div id={visibleButton ? 'noButton' : 'yesButton'}>
+        <button id={invertedButton ? 'invertedButton' : 'welcomeButton'} onClick={handleClick}>Click me to begin!</button>
       </div>
     </div>
   )
