82977aaab090633f53e93cbdd32cd2cdced51a4b
Daniel Williams <danielpaulwill@gmail.com>
2022-12-13 22:37:02 -0500

    updated forest description

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,7 +3,12 @@ class LocationsController < ApplicationController
   def create
     user = User.find_by(id: session[:user_id])
     # user = User.find(4)
-    forest = Location.create(character_id: user.character.id, name: "Forest", description: "A deep green, lush rainforest. It seems inviting, yet you can feel it holds danger.", is_complete: false)
+    forest = Location.create(character_id: user.character.id, name: "Forest", description: "You find yourself in a dark, thick forest. The tall trees block out most of the light and the underbrush is dense and tangled. The air is heavy with the smell of damp earth and decaying leaves, and the ground is covered in a thick layer of moss and fallen branches. The forest is alive with the sound of chirping birds and rustling leaves, but there are also other, more sinister noises - the snarl of a predator, the hiss of a venomous snake, the eerie wail of a banshee.
+
+    The trees in this forest are ancient and massive, with thick trunks and branches that stretch high into the sky. The canopy is so dense that it is difficult to see more than a few feet ahead, creating a sense of unease and isolation.
+    
+    Despite the dangers, the dark, thick forest is also a place of great beauty and wonder. The vibrant green of the leaves, the sparkling streams, and the delicate wildflowers all contribute to its charm. It is a place of solitude and contemplation. Adventurers who are brave enough to venture into this forest may find hidden treasures, lost secrets, or most importantly, answers; but they must be prepared to face the dangers that lurk within its depths.
+    ", is_complete: false)
     volcano = Location.create(character_id: user.character.id, name: "Volcano", description: "A giant, smoking mountain lay before you. It feels warmer the closer you get to it.", is_complete: false)
     # byebug
     locations = Location.all
