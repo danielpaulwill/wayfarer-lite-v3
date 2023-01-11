227587166d58dcb1a2009a72045fbc055def618c
Daniel Williams <danielpaulwill@gmail.com>
2023-01-11 17:25:24 -0500

    Updated two forest events

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,16 @@ user = User.create(username: "danielpaulw", password: "Test1")
 character = Character.create(name: "Ivan", avatar: "https://raw.githubusercontent.com/danielpaulwill/wayfarer-lite-v3/main/client/src/assets/characters/archer.png", health: 50, evil: 50, strength: 50, defense: 50, luck: 50, user_id: user.id)
 
 # LOCATIONS
-forest = Location.create(character_id: character.id, name: "Forest", description: "You find yourself in a dark, thick forest. The tall trees block out most of the light and the underbrush is dense and tangled. The air is heavy with the smell of damp earth and decaying leaves, and the ground is covered in a thick layer of moss and fallen branches. The forest is alive with the sound of chirping birds and rustling leaves, but there are also other, more sinister noises - the snarl of a predator, the hiss of a venomous snake, the eerie wail of a banshee.
-The trees in this forest are ancient and massive, with thick trunks and branches that stretch high into the sky. The canopy is so dense that it is difficult to see more than a few feet ahead, creating a sense of unease and isolation.    
-Despite the dangers, the dark, thick forest is also a place of great beauty and wonder. The vibrant green of the leaves, the sparkling streams, and the delicate wildflowers all contribute to its charm. It is a place of solitude and contemplation. Adventurers who are brave enough to venture into this forest may find hidden treasures, lost secrets, or most importantly, answers; but they must be prepared to face the dangers that lurk within its depths.
+forest = Location.create(character_id: character.id, name: "Forest", description: "You find yourself in a dark, thick forest. The tall trees block out most of the light and the underbrush is dense and tangled. The air is heavy with the smell of damp earth and decaying leaves, and the ground is covered in a thick layer of moss and fallen branches. The forest is alive with the sound of chirping birds and rustling leaves, but there are also other, more sinister noises - the snarl of a predator, the hiss of a venomous snake, the eerie wail of something up in the branches.
+The trees in this forest are ancient and massive, with thick trunks and branches that stretch high into the sky. The canopy is so dense that it is difficult to see more than a few feet ahead, creating a sense of unease and isolation.
 ", is_complete: false)
 volcano = Location.create(character_id: character.id, name: "Volcano", description: "A giant, smoking mountain lay before you. It feels warmer the closer you get to it.", is_complete: false)
 beach = Location.create(character_id: character.id, name: "Beach", description: "It's a beach.", is_complete: false)
 ruins = Location.create(character_id: character.id, name: "Ruins", description: "There are ruins.", is_complete: false)
 
 # EVENTS
-f1 = Event.create(location_id: forest.id, name: "Event One", description: "The first Forest event.", is_complete: false)
-f2 = Event.create(location_id: forest.id, name: "Event Two", description: "The second Forest event.", is_complete: false)
+f1 = Event.create(location_id: forest.id, name: "Through the mist you see several cages with odd looking creatures in them.", description: "The first Forest event.", is_complete: false)
+f2 = Event.create(location_id: forest.id, name: "In the distance you hear someone calling out for help.", description: "The second Forest event.", is_complete: false)
 f3 = Event.create(location_id: forest.id, name: "Event Three", description: "The third Forest event.", is_complete: false)
 f4 = Event.create(location_id: forest.id, name: "Event Four", description: "The fourth Forest event.", is_complete: false)
 
