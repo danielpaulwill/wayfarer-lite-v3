c7b49dd57067210c3f72a9ec3797235f35345a36
Daniel Williams <danielpaulwill@gmail.com>
2023-01-11 23:02:33 -0500

    updated more events

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,10 @@ user = User.create(username: "danielpaulw", password: "Test1")
 character = Character.create(name: "Ivan", avatar: "https://raw.githubusercontent.com/danielpaulwill/wayfarer-lite-v3/main/client/src/assets/characters/archer.png", health: 50, evil: 50, strength: 50, defense: 50, luck: 50, user_id: user.id)
 
 # LOCATIONS
-forest = Location.create(character_id: character.id, name: "Forest", description: "You find yourself in a dark, thick forest. The tall trees block out most of the light and the underbrush is dense and tangled. The air is heavy with the smell of damp earth and decaying leaves, and the ground is covered in a thick layer of moss and fallen branches. The forest is alive with the sound of chirping birds and rustling leaves, but there are also other, more sinister noises - the snarl of a predator, the hiss of a venomous snake, the eerie wail of something up in the branches.;; The trees in this forest are ancient and massive, with thick trunks and branches that stretch high into the sky. The canopy is so dense that it is difficult to see more than a few feet ahead, creating a sense of unease and isolation.
+forest = Location.create(character_id: character.id, name: "Forest", description: "You find yourself in a dark, thick forest. The tall trees block out most of the light and the underbrush is dense and tangled. The air is heavy with the smell of damp earth and decaying leaves, and the ground is covered in a thick layer of moss and fallen branches. The forest is alive with the sound of chirping birds and rustling leaves, but there are also other, more sinister noises - the snarl of a predator, the hiss of a venomous snake, the eerie wail of something up in the branches.;;The trees in this forest are ancient and massive, with thick trunks and branches that stretch high into the sky. The canopy is so dense that it is difficult to see more than a few feet ahead, creating a sense of unease and isolation.
 ", is_complete: false)
-volcano = Location.create(character_id: character.id, name: "Volcano", description: "A giant, smoking mountain lay before you. It feels warmer the closer you get to it.", is_complete: false)
+volcano = Location.create(character_id: character.id, name: "Volcano", description: "The volcano is massive, with ashen gray slopes rising up into the sky. It looks like a giant, sleeping beast, with streams of fire and molten lava spilling out of its mouth. The heat from the volcano is intense, making the air shimmer and the ground hot to the touch.;;As the lava flows down the slopes of the volcano, it creates rivers of fire, lighting up the dark night sky. The ground trembles and shakes as the volcano spews ash and smoke into the air, creating a thick and dangerous fog.
+", is_complete: false)
 beach = Location.create(character_id: character.id, name: "Beach", description: "It's a beach.", is_complete: false)
 ruins = Location.create(character_id: character.id, name: "Ruins", description: "There are ruins.", is_complete: false)
 
@@ -17,7 +18,7 @@ f2 = Event.create(location_id: forest.id, name: "In the distance you hear someon
 f3 = Event.create(location_id: forest.id, name: "Event Three", description: "The third Forest event.", is_complete: false)
 f4 = Event.create(location_id: forest.id, name: "Event Four", description: "The fourth Forest event.", is_complete: false)
 
-v1 = Event.create(location_id: volcano.id, name: "Event One", description: "The first Volcano event.", is_complete: false)
+v1 = Event.create(location_id: volcano.id, name: "You see a group of people gathered around a giant crystal", description: "What will you do?", is_complete: false)
 v2 = Event.create(location_id: volcano.id, name: "Event Two", description: "The second Volcano event.", is_complete: false)
 v3 = Event.create(location_id: volcano.id, name: "Event Three", description: "The third Volcano event.", is_complete: false)
 v4 = Event.create(location_id: volcano.id, name: "Event Four", description: "The fourth Volcano event.", is_complete: false)
