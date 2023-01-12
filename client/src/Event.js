0933f49ca80827bd4bda5f0d341b4a2b20977acf
Daniel Williams <danielpaulwill@gmail.com>
2023-01-11 20:31:24 -0500

    testing splitting up sql strings

--- client/src/Event.js
+++ client/src/Event.js
@@ -3,6 +3,9 @@ import OptionSelect from "./OptionSelect";
 
 function Event({ event, onOptionSelect, character }) {
 
+  let description = event.description.split(';')
+  console.log({description})
+
   // console.log({ event.options })
   let options = event.options.map((option) => <OptionSelect character={character} key={option.id} option={option} onOptionSelect={onOptionSelect} />)
   
@@ -10,7 +13,8 @@ function Event({ event, onOptionSelect, character }) {
     <div className="center">
       <div>
         <h2>{event.name}</h2>
-        <p>{event.description}</p>
+        {/* <p>{event.description}</p> */}
+        <p>{description[0]}<br></br>{description[1]}</p>
         {options}
       </div>
     </div>
