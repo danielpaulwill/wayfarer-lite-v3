008f7d146715c0cbf0705da95c4be00dff202cad
Daniel Williams <danielpaulwill@gmail.com>
2023-01-11 23:07:38 -0500

    updated volcano event 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ f3 = Event.create(location_id: forest.id, name: "Event Three", description: "The
 f4 = Event.create(location_id: forest.id, name: "Event Four", description: "The fourth Forest event.", is_complete: false)
 
 v1 = Event.create(location_id: volcano.id, name: "You see a group of people gathered around a giant crystal", description: "What will you do?", is_complete: false)
-v2 = Event.create(location_id: volcano.id, name: "Event Two", description: "The second Volcano event.", is_complete: false)
+v2 = Event.create(location_id: volcano.id, name: "Through a thick layer of ash you see a hidden passage at the base of the volcano", description: "The second Volcano event.", is_complete: false)
 v3 = Event.create(location_id: volcano.id, name: "Event Three", description: "The third Volcano event.", is_complete: false)
 v4 = Event.create(location_id: volcano.id, name: "Event Four", description: "The fourth Volcano event.", is_complete: false)
 
