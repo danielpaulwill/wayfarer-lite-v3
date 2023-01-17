03576708cf01c5aa8e82d7a7cf8af6327e93a7f0
Daniel Williams <danielpaulwill@gmail.com>
2023-01-16 20:33:38 -0500

    working on changing is_complete on location

--- client/src/ChooseYourLocation.js
+++ client/src/ChooseYourLocation.js
@@ -2,12 +2,37 @@ import React, { useEffect, useState } from "react";
 
 function ChooseYourLocation({ onLocationSelect }) {
   const [forestDone, setForestDone] = useState(false)
+  const [volcanoDone, setVolcanoDone] = useState(false)
+  const [beachDone, setBeachDone] = useState(false)
+  const [ruinsDone, setRuinsDone] = useState(false)
   const [locationData, setLocationData] = useState('')
   const [forestData, setForestData] = useState('')
   const [volcanoData, setVolcanoData] = useState('')
   const [beachData, setBeachData] = useState('')
   const [ruinsData, setRuinsData] = useState('')
 
+  // console.log(forestData.events)
+
+  useEffect(() => {
+    let f = 0
+    let v = 0
+    let b = 0
+    let r = 0
+    forestData.events.map(event => {
+      if (event.is_complete === true){
+        ++f
+      }
+    })
+    console.log(f)
+  }, [ruinsData])
+
+  useEffect(() => {
+    setForestDone(forestData.is_complete)
+    setVolcanoDone(volcanoData.is_complete)
+    setBeachDone(beachData.is_complete)
+    setRuinsDone(ruinsData.is_complete)
+  }, [ruinsData])
+  
   useEffect(() => {
     fetch('/locations')
     .then((res) => res.json())
@@ -15,9 +40,21 @@ function ChooseYourLocation({ onLocationSelect }) {
   }, [])
 
   useEffect(() => {
-    // if locationData {
-    //   locationData.map(location => console.log({location}))
-    // }
+    if (locationData === '') {
+      console.log('no location data')
+    } else {
+      locationData.map(location => {
+        if (location.name === 'Forest'){
+          setForestData(location)
+        } else if (location.name === 'Volcano'){
+          setVolcanoData(location)
+        } else if (location.name === 'Beach'){
+          setBeachData(location)
+        } else if (location.name === "Ruins"){
+          setRuinsData(location)
+        }
+      })
+    }
   }, [locationData])
 
   
