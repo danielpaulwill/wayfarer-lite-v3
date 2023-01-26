70aa5626cd827ee5b5a232ddf716ca718029f25a
Daniel Williams <danielpaulwill@gmail.com>
2023-01-25 23:52:37 -0500

    need to implement i incrementer to slow down and end roll function

--- client/src/DieRoll.js
+++ client/src/DieRoll.js
@@ -4,21 +4,27 @@ function DieRoll () {
   const [roll, setRoll] = useState()
 
   function rollDice() {
-    return Math.floor(Math.random() * 4) + 1;
+    setRoll(Math.ceil(Math.random() * 20))
   }
+
+  function gameShow() {
+    let i = 0
+    if (i < 10) {
+      setInterval(rollDice, 10)
+    // } else if (i >)
+  }}
   
   useEffect(() => {
     let rollOne = rollDice()
     setRoll(rollOne)
   }, [])
-
   
   return (
     <div className="center">
       <div>
         <h1>DieRoll</h1>
         <h2>{roll}</h2>
-        <button className="normalButton" onClick={e => setRoll(rollDice())}>Roll Again</button>
+        <button className="normalButton" onClick={e => gameShow()}>Roll Again</button>
       </div>
     </div>
   )
