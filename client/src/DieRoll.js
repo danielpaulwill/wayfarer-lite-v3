d6b0ce81dff7ea8ce84f02a4da09e0c819b23af4
Daniel Williams <danielpaulwill@gmail.com>
2023-01-25 23:17:42 -0500

    function written

--- client/src/DieRoll.js
+++ client/src/DieRoll.js
@@ -1,8 +1,19 @@
 function DieRoll () {
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  
+  let roll = getRandomInt(1, 21)
+  
   
   return (
     <div className="center">
       <h1>DieRoll</h1>
+      <br></br>
+      <h3>{roll}</h3>
     </div>
   )
 };
