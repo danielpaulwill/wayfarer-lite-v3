a5a1c46a808ed8cfbe0637f91208a6eb7f9a1f40
Daniel Williams <danielpaulwill@gmail.com>
2023-02-09 20:19:11 -0500

    adding conversation with demon

---
+++ client/src/EvilReveal.js
@@ -0,0 +1,14 @@
+function EvilReveal () {
+
+  
+  return (
+    <div id="dice">
+      <div>
+        <h2>{roll}</h2>
+        <button className="normalButton" onClick={e => rollDice()}>Roll Dice</button>
+      </div>
+    </div>
+  )
+};
+
+export default EvilReveal;
